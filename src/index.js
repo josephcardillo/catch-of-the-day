7380c3f864dcfc6cdadf63060843314feacfd416
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-30 17:24:57 -0400

    Learning Routing.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './css/style.css';
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
